d13d5e1df273426dd76111479b3a7f69bbbe831e
mariklim <maria.klimenko889@gmail.com>
2021-11-05 17:33:08 +0100

    add base for header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,52 @@
-<header>header</header>
+<header>
+    <div class="header-top">
+        <header-top-nav>
+            <a href="#">DC POWER Visa</a>
+            <a href="#">Adittionals dc sites</a>
+        </header-top-nav>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+          <div class="logo">
+              <img src="{{asset('images/dc-logo.png')}}" alt="logo">
+          </div>
+          <div class="header-bottom-nav">
+              <ul>
+                  <li>
+                      <a href="#">CHARACTERS</a>
+                  </li>
+                  <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDIOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOPS</a>
+                </li>
+              </ul>
+          </div>
+          <div class="header-bototm-search">
+              <input type="text" placeholder="Search">
+          </div>
+        </div>
+    </div>
+</header>
