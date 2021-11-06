88344ba083b539cb3782127a96dca2b43aa63d0b
mariklim <maria.klimenko889@gmail.com>
2021-11-06 17:35:50 +0100

    add merchandise section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,4 +21,42 @@
             LOAD MORE
         </button>
     </section>
+    <section class="merchandise">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li>
+                         <a class="link-merchandise" href="#">
+                                <img src="{{asset('images/buy-comics-digital-comics.png')}}" alt="buy-comics-digital-comics">
+                                <span>DIGITAL COMICS</span>
+                            </a>
+                     </li>
+                     <li>
+                        <a class="link-merchandise" href="#">
+                               <img src="{{asset('images/buy-comics-merchandise.png')}}" alt="buy-comics-merchandise">
+                               <span>DC MERCHANDISE</span>
+                           </a>
+                    </li>
+                    <li>
+                        <a class="link-merchandise" href="#">
+                               <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="buy-comics-subscriptions">
+                               <span>SUBSCRIPTION</span>
+                           </a>
+                    </li>
+                    <li>
+                        <a class="link-merchandise" href="#">
+                               <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="buy-comics-shop-locator">
+                               <span>COMICS SHOP LOCATOR</span>
+                           </a>
+                    </li>
+                    <li>
+                        <a class="link-merchandise" href="#">
+                               <img src="{{asset('images/buy-dc-power-visa.svg')}}" alt="buy-dc-power-visa">
+                               <span>CD POWER VISA</span>
+                           </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </section>
 @endsection
