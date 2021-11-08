c4be42aa2af03ae8118022b1eebf07c39ed023bd
mariklim <maria.klimenko889@gmail.com>
2021-11-08 08:45:15 +0100

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,88 @@
-<footer>footer</footer>
+<footer>
+    <div class="footer-top">
+        <!-- Footer menu -->
+        <div class="container">
+          <div class="col">
+            <h2>DC COMICS</h2>
+            <ul>
+              <li>Characters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>TV</li>
+              <li>Games</li>
+              <li>Videos</li>
+              <li>News</li>
+            </ul>
+  
+            <h2>DC SHOP</h2>
+            <ul>
+              <li>Characters</li>
+              <li>Comics</li>
+            </ul>
+          </div>
+  
+          <div class="col">
+            <h2>DC</h2>
+            <ul>
+              <li>Terms Of Use</li>
+              <li>Privacy Policy (New)</li>
+              <li>Ad Choices</li>
+              <li>Advertising</li>
+              <li>Jobs</li>
+              <li>Subscriptions</li>
+              <li>CPSC Certificates</li>
+              <li>Ratings</li>
+              <li>Shop Help</li>
+              <li>Contact Us</li>
+            </ul>
+  
+          </div>
+          <div class="col">
+            <h2>SITES</h2>
+            <ul>
+              <li>DC</li>
+              <li>DC UNIVERSE INFINITE</li>
+              <li>DC Kids</li>
+              <li>DC Shop</li>
+              <li>MAD Magazine</li>
+            </ul>
+          </div>
+        </div>
+        <div class="container" >
+            <p class="copyright">
+                All Site Content TM and © 2020 DC Entertainment, unless otherwise 
+               <span class="copyright-active">noted here</span>. All rights reserved. 
+<span class="copyright-active"> Cookies Settings</span>
+               
+            </p>
+        </div>
+        <!-- /Footer menu -->
+    </div>
+      <div class="footer-bottom">
+        <div class="container">
+          <button class="signUp">SIGN-UP NOW!</button>
+          <div class="social-CTA">
+            <span>FOLLOW US</span>
+            <span class="icons-social">
+              <ul>
+                <li>
+                  <img src="{{asset('images/footer-facebook.png')}}" alt="facebook" />
+                </li>
+                <li>
+                    <img src="{{asset('images/footer-twitter.png')}}" alt="twitter" />
+                </li>
+                <li>
+                    <img src="{{asset('images/footer-youtube.png')}}" alt="youtube" />
+                </li>
+                <li>
+                    <img src="{{asset('images/footer-pinterest.png')}}" alt="pinterest" />
+                </li>
+                <li>
+                    <img src="{{asset('images/footer-periscope.png')}}" alt="periscope" />
+                </li>
+              </ul>
+            </span>
+          </div>
+        </div>
+      </div>
+</footer>
